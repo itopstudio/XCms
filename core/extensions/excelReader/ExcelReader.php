f450d4a796f8764e0c3243176f851e361db49c7e
lancelot <lancelot1215@gmail.com>
2013-10-06 04:35:29 +0800

    x

---
+++ core/extensions/excelReader/ExcelReader.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @name ExcelReader.php UTF-8
+ * @author ChenJunAn<lancelot1215@gmail.com>
+ * 
+ * Date 2013-10-6
+ * Encoding UTF-8
+ */
+require_once dirname(__FILE__).DIRECTORY_SEPARATOR.'excel_reader2.php';
+
+class ExcelReader extends Spreadsheet_Excel_Reader
+{
+}
